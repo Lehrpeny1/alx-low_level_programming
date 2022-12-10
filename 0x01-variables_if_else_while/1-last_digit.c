ccb6f65517b568345ec2832a0a7a8ea7e4af4693
lehrpeny <oladeleolanrewaju6@gmail.com>
2022-12-10 07:56:04 -0800

    second

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,34 @@
-#include <stdlib>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/*betty style doc for function main goes there */
+/**
+ * main - main function to generate ramdom number
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
 int main(void)
 {
-        int n;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+	printf("Last digit of %d is ", n);
+	if (n > 5)
+	{
+		printf("%d amd is greaterthan 5\n", n);
+	}
+	else if ((n < 6) && (n < 0))
+	{
+		printf("%d and is less than 6and not 0\n", n);
+	}
+	else
+	{
+		printf("%d and is 0\n", n);
+	}
+	return (0);
+}
+
+
