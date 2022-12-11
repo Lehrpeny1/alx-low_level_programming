3a6a8def1eacfe1103785d126891004125ff986f
lehrpeny <oladeleolanrewaju6@gmail.com>
2022-12-11 10:06:57 -0800

    forth

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function to generate ramdom number
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int ch = 'a';
+	int CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
+
