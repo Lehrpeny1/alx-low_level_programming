51586ce6884737c6eae416b5b5dd322eece684bf
lehrpeny <oladeleolanrewaju6@gmail.com>
2022-12-12 14:27:03 -0800

    eleventh

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function to generate ramdom number
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = 0; tens <= '0'; tens++)
+	{
+		for (ones = '0'; ones <= '0'; ones++)
+		{
+			if (!((ones == tens) || (tens > ones)))
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '0' && tens == '0'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	
+	putchar(10);
+	
+	return (0);
+	
+}
