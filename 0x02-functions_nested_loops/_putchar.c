8063fff123858b5f18cd989178a832c96d35c1a2
lehrpeny <oladeleolanrewaju6@gmail.com>
2022-12-13 16:27:46 -0800

    third

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,7 +4,7 @@
  * _putchar - write the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1 .
+ * Return: On success 1,
  * on error, -1 is returned, and errno 1s set appropriately.
  */
 int _putchar(char c)
