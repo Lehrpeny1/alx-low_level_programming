747b9f454b85f26211571bd6e6963366b4377463
lehrpeny <oladeleolanrewaju6@gmail.com>
2023-01-12 14:36:30 -0800

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,11 +41,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0';
 			str[i] = s1[i];
 	/*add s2 n bytes to str*/
-	for (j = 0; j < n && s2[j] != '\0'; j++)
-	{
+		for (j = 0; j < n && s2[j] != '\0'; j++)
+		{
 		str[i] = s2[j];
 		i++;
-	}
+		}
 	str[i] = '\0';
 	return (str);
 }
