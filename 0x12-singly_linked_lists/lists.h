20cf9b10d2f198cb90ec1f11983f9739c73adeee
lehrpeny <oladeleolanrewaju6@gmail.com>
2023-01-26 14:44:06 -0800

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 
 /**
@@ -25,4 +29,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-endif
+#endif
